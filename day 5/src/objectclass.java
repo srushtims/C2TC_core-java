8f9abdb360236f8b6302d2771cf491d9a4a07706
user <user@user-PC>
2022-06-04 10:26:53 +0530

    first commit

---
+++ day 5/src/objectclass.java
@@ -0,0 +1,14 @@
+
+public class objectclass {
+int x;
+	public static void main(String[] args) {
+		objectclass myobj1 = new objectclass();
+		objectclass myobj2 = new objectclass();
+		myobj1.x = 24;
+		myobj2.x = 55;
+		System.out.println(myobj1.x);
+		System.out.println(myobj2.x);
+		
+	}
+
+}
