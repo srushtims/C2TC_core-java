f39f1e1dbe8ce71dd0cdc6cc749dfb0662f3a03d
user <user@user-PC>
2022-06-09 17:02:15 +0530

    first commit

---
+++ day 6/src/com/TNS/understandpackage/Understanding.java
@@ -0,0 +1,5 @@
+package com.TNS.understandpackage;
+
+public class Understanding {
+
+}
---
+++ day 6/src/com/acharya/am1/test1.java
@@ -0,0 +1,15 @@
+package com.acharya.am1;
+
+public class test1 {
+private int i=10;
+private void show() {
+	System.out.println("Hello world");
+}
+	public static void main(String[] args) {
+		test1 t1=new test1();
+		t1.show();
+		System.out.println(t1.i);
+
+	}
+
+}
